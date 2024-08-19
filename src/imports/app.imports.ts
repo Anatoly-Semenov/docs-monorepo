a575a081cb9b90ddbed2f26aac983a952a685e60
Anatoly Semenov <anatolysrmenov@gmail.com>
2024-08-19 17:22:28 +0300

    feat: init imports

---
+++ src/imports/app.imports.ts
@@ -0,0 +1,4 @@
+import { BusinessLogicImports } from "./business-logic.imports"
+import { CommonImports, Imports } from "./common.imports"
+
+export const AppImports: Imports = [...CommonImports, ...BusinessLogicImports]
